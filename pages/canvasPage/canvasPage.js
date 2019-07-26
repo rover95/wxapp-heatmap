930be470a88cc3717944e83a6c93b20569a45d82
rover95 <rover95rx@gmail.com>
2019-07-26 16:34:24 +0800

    加入地图，按钮缩放

--- pages/canvasPage/canvasPage.js
+++ pages/canvasPage/canvasPage.js
@@ -2,38 +2,66 @@
 import colorGradual from "./colorGradual";
 
 let p_width = 375;
-let p_height = 200;
+let p_height = 300;
+const range = 60;
+
+let t ;
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    p_width,
+    p_height,
+    mapScale: 7
   },
-
+  ctx: wx.createCanvasContext("cvs"),
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.drawCanvas();
+  onLoad: function(options) {
+    const systemInfo = wx.getSystemInfoSync();
+    const mapCtx = wx.createMapContext("map");
+    p_width = systemInfo.screenWidth;
+    this.mapCtx = mapCtx;
+    this.getMapRegion(()=>{
+      this.points = this.getMapPoints();
+      this.drawCanvas(this.points);
+    });
     // this.getColorGradual();
   },
+  //获取窗口坐标范围
+  getMapRegion(fun){
+    this.mapCtx.getRegion({
+      complete: res => {
+        let northwest = [res.southwest.longitude, res.northeast.latitude];
+        let southeast = [res.northeast.longitude, res.southwest.latitude];
+        this.northwest = northwest;
+        this.southeast = southeast;
+        this.scale_x = p_width / (southeast[0] - northwest[0]);
+        this.scale_y = p_height / (southeast[1] - northwest[1]);
+        if(fun){
+          fun()
+        }
+        
+      }
+    });
+  },
   // 绘制彩色渐变条
-  getColorGradual(){
+  getColorGradual() {
     let ctx = wx.createCanvasContext("color");
-    let grd = ctx.createLinearGradient(0,0,256,10);
-    grd.addColorStop(0.2, 'rgba(0,0,255,0.2)')
+    let grd = ctx.createLinearGradient(0, 0, 256, 10);
+    grd.addColorStop(0.2, "rgba(0,0,255,0.2)");
     grd.addColorStop(0.3, "rgba(43,111,231,0.3)");
     grd.addColorStop(0.4, "rgba(2,192,241,0.4)");
     grd.addColorStop(0.6, "rgba(44,222,148,0.6)");
     grd.addColorStop(0.8, "rgba(254,237,83,0.8)");
     grd.addColorStop(0.9, "rgba(255,118,50,0.9)");
-    grd.addColorStop(1, 'rgba(255,10,0,1)');
+    grd.addColorStop(1, "rgba(255,10,0,0.95)");
     ctx.setFillStyle(grd);
     ctx.fillRect(0, 0, 256, 10);
-    ctx.draw(true,()=>{
+    ctx.draw(true, () => {
       wx.canvasGetImageData({
         canvasId: "color",
         x: 0,
@@ -41,39 +69,51 @@ Page({
         width: 256,
         height: 1,
         complete: res => {
-          console.log(res.data.toString());
+          console.log(res.data.toString()); //用于生成colorGradual.js
           // this.drawCanvas();
         }
       });
-    })
+    });
   },
-  drawCanvas(){
-    let ctx = wx.createCanvasContext("cvs");
-    ctx.clearRect(0, 0, p_width, p_height);
+  // 创建热力点
+  getMapPoints() {
+    let points = [];
     let i = 0;
-    while (i<20) {
-      i++
-      let x = Math.random() * p_width;
-      let y = Math.random() * p_height;
-      let grd = ctx.createCircularGradient(x, y, 55)
-      grd.addColorStop(0, '#000000')
-      grd.addColorStop(1, 'rgba(0,0,0,0)')
-      ctx.setFillStyle(grd)
-      ctx.arc(x,y, 55, 0, 2 * Math.PI);
-      ctx.fill()
-      
+    while (i < 40) {
+      i++;
+      let x = 110 + ((Math.random() - 0.5) * 15) / 2;
+      let y = 30 + ((Math.random() - 0.5) * 5) / 2;
+      points.push({
+        longitude: x,
+        latitude: y,
+        ratio: Math.random()
+      });
     }
+    return points;
+  },
+  // 绘制叠加点透明通道
+  drawCanvas(points) {
+    t = new Date().getTime();
+    let ctx = this.ctx;
+    ctx.clearRect(0, 0, p_width, p_height);
+    points.map(val => {
+      let { ratio } = val;
+      let x = (val.longitude - this.northwest[0]) * this.scale_x;
+      let y = (val.latitude - this.northwest[1]) * this.scale_y;
+      let grd = ctx.createCircularGradient(x, y, range * ratio);
+      grd.addColorStop(0, "#000000");
+      grd.addColorStop(1, "rgba(0,0,0,0)");
+      ctx.setFillStyle(grd);
+      ctx.arc(x, y, range * ratio, 0, 2 * Math.PI);
+      ctx.fill();
+    });
+
     ctx.draw(true, () => {
-      // return
       this.drawGradual("cvs");
     });
-    
-    // ctx.fillRect(0, 0, 500, 5000)
-    // ctx.stroke()
-    // ctx.draw()
   },
   // 绘制渐变映射
-  drawGradual(id){
+  drawGradual(id) {
     wx.canvasGetImageData({
       canvasId: id,
       x: 0,
@@ -82,11 +122,6 @@ Page({
       height: p_height,
       complete: res => {
         let imgData = res.data;
-        // console.log(colorGradual);
-        // console.log(imgData);
-
-        // return
-        let t = new Date().getTime();
         for (let i = 3; i < imgData.length; i += 4) {
           let j = imgData[i] * 4;
           if (!j) {
@@ -104,63 +139,68 @@ Page({
           width: p_width,
           height: p_height,
           complete(res) {
-            console.log(res);
-            console.log(new Date().getTime() - t);
+            console.log("渲染用时：" + (new Date().getTime() - t) + "ms");
           }
         });
       }
     });
   },
-  
-  handleResetBtn(){
-    this.drawCanvas();
-  } , 
+  //清空画布
+  clearCanvas(){
+    this.ctx.clearRect(0, 0, p_width, p_height);
+  },
+  //map视野变化
+  handleRegionchange(e) {
+    if (!this.points || e.type != "end") {
+      return;
+    }
+    this.getMapRegion(()=>{
+      this.drawCanvas(this.points);
+    });
+  },
+  handleResetBtn() {
+    let points = this.getMapPoints();
+    this.drawCanvas(points);
+  },
+  handleScaleChange(e) {
+    let { id } = e.currentTarget;
+    let num = id == "less" ? -1 : 1;
+    this.setData({
+      mapScale: this.data.mapScale + num
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
--- readme.md
+++ readme.md
@@ -6,6 +6,6 @@
 
 后续会考虑和map组件叠加，在地图上显示热力图
 
-![img](https://raw.githubusercontent.com/rover95/image/master/img/heatmap.png)
+![img](https://raw.githubusercontent.com/rover95/image/master/img/heatmap3.png)
 
 小程序画布性能孱弱，热力图需要像素级操作，全屏或渲染大尺寸热力图会有卡顿，有待微信优化  
